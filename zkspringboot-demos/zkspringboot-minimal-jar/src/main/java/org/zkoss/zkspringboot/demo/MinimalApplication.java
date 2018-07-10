351666c8e3217dd5ae0ffaa196082a0793a7116b
Robert Wenzel <robertwenzel@potix.com>
2018-07-10 17:50:37 +0800

    rename modules

---
+++ zkspringboot-demos/zkspringboot-minimal-jar/src/main/java/org/zkoss/zkspringboot/demo/MinimalApplication.java
@@ -0,0 +1,19 @@
+package org.zkoss.zkspringboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class MinimalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalApplication.class);
+	}
+	
+	@GetMapping("/")
+	public String demo() {
+		return "zul/minimal";
+	}
+}

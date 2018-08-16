dfd10ec301e4680cb2ec38fa3040638837fa767c
Robert Wenzel <robertwenzel@potix.com>
2018-08-16 18:44:28 +0800

    split upload folder

--- prepare-upload.sh
+++ prepare-upload.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 VERSION=$1
 
-UPLOAD_FOLDER=./target/to_upload/$VERSION
-MVN_FOLDER=$UPLOAD_FOLDER/maven
+UPLOAD_FOLDER=./target/to_upload
+MVN_FOLDER=$VERSION/maven
+
+ZSB_STARTER_FOLDER=$UPLOAD_FOLDER/zkspringboot-starter/$MVN_FOLDER
+ZSB_ZATS_FOLDER=$UPLOAD_FOLDER/zkspringboot-zats/$MVN_FOLDER
 
 rm -rf $UPLOAD_FOLDER
-mkdir -p $UPLOAD_FOLDER
-mkdir -p $MVN_FOLDER
 
-cp zkspringboot-starter/target/zkspringboot-starter-$VERSION-bundle.jar $MVN_FOLDER
-cp zkspringboot-zats/target/zkspringboot-zats-$VERSION-bundle.jar $MVN_FOLDER
+mkdir -p $ZSB_STARTER_FOLDER
+mkdir -p $ZSB_ZATS_FOLDER
+
+cp zkspringboot-starter/target/zkspringboot-starter-$VERSION-bundle.jar $ZSB_STARTER_FOLDER
+cp zkspringboot-zats/target/zkspringboot-zats-$VERSION-bundle.jar $ZSB_ZATS_FOLDER

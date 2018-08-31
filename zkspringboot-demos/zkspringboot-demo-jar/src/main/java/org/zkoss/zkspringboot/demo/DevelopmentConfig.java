d109760a9145ee713d8a9ba0f5abafba897b6712
Robert Wenzel <robertwenzel@potix.com>
2018-08-31 11:49:57 +0800

    add alternative jetty/undertow config, dev config label reloading

--- zkspringboot-demos/zkspringboot-demo-jar/src/main/java/org/zkoss/zkspringboot/demo/DevelopmentConfig.java
+++ zkspringboot-demos/zkspringboot-demo-jar/src/main/java/org/zkoss/zkspringboot/demo/DevelopmentConfig.java
@@ -5,7 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.zkoss.lang.Library;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.zk.ui.Desktop;
 import org.zkoss.zk.ui.WebApps;
+import org.zkoss.zk.ui.util.DesktopInit;
 
 import javax.annotation.PostConstruct;
 
@@ -15,14 +18,32 @@ public class DevelopmentConfig {
 	private static Logger logger = LoggerFactory.getLogger(DevelopmentConfig.class);
 
 	@PostConstruct
-	public void initDevelopmentProperties() {
+	public void initDevelopmentProperties() throws Exception {
 		logger.info("**************************************************************");
 		logger.info("**** ZK-Springboot-Demo: development configuration active ****");
 		logger.info("**************************************************************");
-		WebApps.getCurrent().getConfiguration().setDebugJS(true);
+
+		//disable various caches to avoid server restarts
 		Library.setProperty("org.zkoss.zk.ZUML.cache", "false");
 		Library.setProperty("org.zkoss.zk.WPD.cache", "false");
 		Library.setProperty("org.zkoss.zk.WCS.cache", "false");
 		Library.setProperty("org.zkoss.web.classWebResource.cache", "false");
+		WebApps.getCurrent().getConfiguration().addListener(DynamicLabelReloader.class);
+
+		// enable non minified js
+		WebApps.getCurrent().getConfiguration().setDebugJS(true);
+
+		// enable for debugging MVVM commands and binding (very verbose)
+		Library.setProperty("org.zkoss.bind.DebuggerFactory.enable", "false");
+	}
+
+	/**
+	 * resets/reloads zk-labels for each page load
+	 */
+	public static class DynamicLabelReloader implements DesktopInit {
+		@Override
+		public void init(Desktop desktop, Object o) throws Exception {
+			Labels.reset();
+		}
 	}
 }

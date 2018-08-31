e21455cdb3e067841f85e082b614ab32f75e13d4
Robert Wenzel <robertwenzel@potix.com>
2018-08-31 17:35:27 +0800

    adjust release scripts

--- prepare-upload.sh
+++ prepare-upload.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 VERSION=$1
 
+set -e
+
 UPLOAD_FOLDER=./target/to_upload
 MVN_FOLDER=$VERSION/maven
 EVAL_FOLDER=EE-eval
 
+ZSB_AUTOCONFIG_PATH=$UPLOAD_FOLDER/zkspringboot-autoconfig/$MVN_FOLDER
 ZSB_STARTER_PATH=$UPLOAD_FOLDER/zkspringboot-starter/$MVN_FOLDER
-ZSB_ZATS_PATH=$UPLOAD_FOLDER/zkspringboot-zats/$MVN_FOLDER
 
 rm -rf $UPLOAD_FOLDER
 
+mkdir -p $ZSB_AUTOCONFIG_PATH/$EVAL_FOLDER
 mkdir -p $ZSB_STARTER_PATH/$EVAL_FOLDER
-mkdir -p $ZSB_ZATS_PATH/$EVAL_FOLDER
 
 cp zkspringboot-starter/target/zkspringboot-starter-$VERSION-bundle.jar $ZSB_STARTER_PATH
 cp zkspringboot-starter/target/zkspringboot-starter-$VERSION-bundle.jar $ZSB_STARTER_PATH/$EVAL_FOLDER
 
-cp zkspringboot-zats/target/zkspringboot-zats-$VERSION-bundle.jar $ZSB_ZATS_PATH
-cp zkspringboot-zats/target/zkspringboot-zats-$VERSION-bundle.jar $ZSB_ZATS_PATH/$EVAL_FOLDER
-
+cp zkspringboot-autoconfig/target/zkspringboot-autoconfig-$VERSION-bundle.jar $ZSB_AUTOCONFIG_PATH
+cp zkspringboot-autoconfig/target/zkspringboot-autoconfig-$VERSION-bundle.jar $ZSB_AUTOCONFIG_PATH/$EVAL_FOLDER

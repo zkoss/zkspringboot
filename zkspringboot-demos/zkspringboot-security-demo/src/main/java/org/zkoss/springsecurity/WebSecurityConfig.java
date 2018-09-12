efd951a81f722ecf235460dbce1bf53db5ee2742
Robert Wenzel <robertwenzel@potix.com>
2018-09-12 18:19:51 +0800

    allow rmDesktop, rename project

--- zkspringboot-demos/zkspringboot-security-demo/src/main/java/org/zkoss/springsecurity/WebSecurityConfig.java
+++ zkspringboot-demos/zkspringboot-security-demo/src/main/java/org/zkoss/springsecurity/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.zkoss.springsecurity;
 
 import org.springframework.context.annotation.*;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.core.userdetails.*;
@@ -9,16 +10,20 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    public static final String ZUL_FILES = "/zkau/web/**/*.zul";
+    public static final String[] ZK_RESOURCES = {"/zkau/web/**/js/**", "/zkau/web/**/zul/css/**", "/zkau/web/**/img/**"};
+    // allow desktop cleanup after logout or when reloading login page
+    public static final String REMOVE_DESKTOP_REGEX = "/zkau\\?dtid=.*&cmd_0=rmDesktop&.*";
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //make zk au work
-        //ZK already has built-in CSRF token when sending a request, please refer to https://www.zkoss.org/wiki/ZK%20Developer's%20Reference/Security%20Tips/Cross-site%20Request%20Forgery
+        // ZK already has built-in CSRF token when sending a request,
+        // please refer to https://www.zkoss.org/wiki/ZK%20Developer's%20Reference/Security%20Tips/Cross-site%20Request%20Forgery
         http.csrf().disable();
         http.authorizeRequests()
-            .antMatchers("/zkau/web/**/js/**","/zkau/web/**/zul/css/**","/zkau/web/**/img/**")
-            .permitAll()
-            // block direct access to class path web resources
-            .antMatchers("/zkau/web/**/**.zul").denyAll()
+            .antMatchers(ZUL_FILES).denyAll() // block direct access to zul files
+            .antMatchers(HttpMethod.GET, ZK_RESOURCES).permitAll() // allow zk resources
+            .regexMatchers(HttpMethod.GET, REMOVE_DESKTOP_REGEX).permitAll() // allow desktop cleanup
             .mvcMatchers("/","/login","/logout").permitAll()
             .mvcMatchers("/secure").hasRole("USER")
             .anyRequest().authenticated()

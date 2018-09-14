24e6dbb60e0e6f0c4ff35a832e0eb6235b424177
Robert Wenzel <robertwenzel@potix.com>
2018-09-14 19:32:36 +0800

    fix zats session cookie handling during spring security login

--- zkspringboot-demos/zkspringboot-security-demo/src/test/java/org/zkoss/zkspringboot/security/SecurePageTest.java
+++ zkspringboot-demos/zkspringboot-security-demo/src/test/java/org/zkoss/zkspringboot/security/SecurePageTest.java
@@ -7,14 +7,14 @@ import org.zkoss.zats.junit.AutoClient;
 import org.zkoss.zats.junit.AutoEnvironment;
 import org.zkoss.zats.mimic.ComponentAgent;
 import org.zkoss.zats.mimic.DesktopAgent;
+import org.zkoss.zats.mimic.QueryAgent;
 import org.zkoss.zats.mimic.impl.EmulatorClient;
 import org.zkoss.zul.Label;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.CookieHandler;
-import java.net.CookieManager;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -23,36 +23,53 @@ import static org.junit.Assert.assertTrue;
 public class SecurePageTest {
 	@ClassRule
 	public static AutoEnvironment env = new AutoEnvironment("src/test/webapp/WEB-INF", "src/test/webapp");
-
 	@Rule
 	public AutoClient client = env.autoClient();
+	@Rule //captures cookies (changing JSESSIONID) after login to sync them into the ZATS client
+	public AutoCookieManager autoCookieManager = new AutoCookieManager(client);
 
-	private DesktopAgent desktopAgent;
+	private ComponentAgent loginButton(QueryAgent qa) {
+		return qa.query("button[label='Login']");
+	}
+
+	private ComponentAgent loginSuccessLabel(QueryAgent qa) {
+		return qa.query("div > label");
+	}
+
+	private String[] loginInfo = {"/login", "user", "password"};
 
 	@Test
-	public void testSecureMainLogin() throws IOException {
-		desktopAgent = client.connect("/secure/main");
-		assertNotNull("should redirect to login page", loginButton());
-		performLogin("user", "password", "/secure/main");
-		desktopAgent = client.connect("/secure/main");
-		assertTrue(loginSuccessLabel().as(Label.class).getValue().contains("You should see this a secure page after a successful authentication."));
+	public void testSecureMainPage() throws IOException {
+		DesktopAgent dt = connectWithLogin("/secure/main", loginInfo);
+		assertTrue(loginSuccessLabel(dt)
+				.as(Label.class).getValue()
+				.contains("You should see this a secure page after a successful authentication."));
 	}
 
 	@Test
-	public void testSecureAnotherLogin() throws IOException {
-		desktopAgent = client.connect("/secure/another");
-		assertNotNull("should redirect to login page", loginButton());
-		performLogin("user", "password", "/secure/main");
-		desktopAgent = client.connect("/secure/another");
-		assertTrue(loginSuccessLabel().as(Label.class).getValue().contains("this is another page"));
+	public void testSecureAnotherPage() throws IOException {
+		DesktopAgent dt = connectWithLogin("/secure/another", loginInfo);
+		assertTrue(loginSuccessLabel(dt)
+				.as(Label.class).getValue()
+				.contains("this is another page"));
 	}
 
-	private void performLogin(final String username, final String password, String path) throws IOException {
-		CookieManager cm = new CookieManager();
-		CookieHandler.setDefault(cm);
+	private DesktopAgent connectWithLogin(String securePath, String[] loginInfo) throws IOException {
+		DesktopAgent dt = client.connect(securePath);
+		assertNotNull("unauthenticated access should redirect to login page", loginButton(dt));
 
-		EmulatorClient innerClient = (EmulatorClient) client.getClient();
-		final HttpURLConnection connection = innerClient.getConnection("/login", "POST");
+		final HttpURLConnection response = postSpringFormLogin(loginInfo[0], loginInfo[1], loginInfo[2]);
+		assertEquals(302, response.getResponseCode());
+		final String redirectPath = new URL(response.getHeaderField("location")).getPath();
+		assertEquals("application should redirect to same path after login", securePath, redirectPath);
+
+		autoCookieManager.syncToZats();
+		return client.connect(redirectPath);
+	}
+
+
+	private HttpURLConnection postSpringFormLogin(String loginPath, final String username, final String password) throws IOException {
+		final HttpURLConnection connection = ((EmulatorClient) client.getClient()).getConnection(loginPath, "POST");
 		connection.setDoOutput(true);
 		connection.setDoInput(true);
 		connection.setInstanceFollowRedirects(false);
@@ -61,18 +78,6 @@ public class SecurePageTest {
 		final OutputStream os = connection.getOutputStream();
 		os.write(("username=" + username + "&password=" + password).getBytes("utf-8"));
 		os.close();
-		CookieHandler.setDefault(null);
-
-		assertEquals(302, connection.getResponseCode());
-		assertTrue(connection.getHeaderField("location"), connection.getHeaderField("location").contains(path));
-
-		cm.getCookieStore().getCookies().stream().forEach(cookie -> innerClient.setCookie(cookie.getName(), cookie.getValue()));
-	}
-
-	private ComponentAgent loginButton() {
-		return desktopAgent.query("button[label='Login']");
-	}
-	private ComponentAgent loginSuccessLabel() {
-		return desktopAgent.query("div > label");
+		return connection;
 	}
 }

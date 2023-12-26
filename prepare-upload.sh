438aefa300bffd9056706b99f63a721e84ff4d57
hawkchen <hawkchen@potix.com>
2023-12-26 16:34:03 +0800

    * get version string from zkspring-starter/pom.xml, not from version * don't create ee eval bundle since this project is for CE

--- prepare-upload.sh
+++ prepare-upload.sh
@@ -5,7 +5,6 @@ set -e
 
 UPLOAD_FOLDER=./target/to_upload
 MVN_FOLDER=$VERSION/maven
-EVAL_FOLDER=EE-eval
 
 ZSB_AUTOCONFIG_PATH=$UPLOAD_FOLDER/zkspringboot-autoconfig/$MVN_FOLDER
 ZSB_STARTER_PATH=$UPLOAD_FOLDER/zkspringboot-starter/$MVN_FOLDER
@@ -14,9 +13,5 @@ rm -rf $UPLOAD_FOLDER
 
 mkdir -p $ZSB_AUTOCONFIG_PATH/$EVAL_FOLDER
 mkdir -p $ZSB_STARTER_PATH/$EVAL_FOLDER
-
 cp zkspringboot-starter/target/zkspringboot-starter-$VERSION-bundle.jar $ZSB_STARTER_PATH
-cp zkspringboot-starter/target/zkspringboot-starter-$VERSION-bundle.jar $ZSB_STARTER_PATH/$EVAL_FOLDER
-
 cp zkspringboot-autoconfig/target/zkspringboot-autoconfig-$VERSION-bundle.jar $ZSB_AUTOCONFIG_PATH
-cp zkspringboot-autoconfig/target/zkspringboot-autoconfig-$VERSION-bundle.jar $ZSB_AUTOCONFIG_PATH/$EVAL_FOLDER

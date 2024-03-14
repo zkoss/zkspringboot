6fbba493e08501706d12bc2f912c96f40879f579
hawkchen <hawkchen@potix.com>
2024-03-14 16:42:17 +0800

    add a page mapping example

--- zkspringboot-demos/zkspringboot-minimal-jar/src/main/java/org/zkoss/zkspringboot/demo/MinimalApplication.java
+++ zkspringboot-demos/zkspringboot-minimal-jar/src/main/java/org/zkoss/zkspringboot/demo/MinimalApplication.java
@@ -6,8 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class MinimalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MinimalApplication.class);
+	}
+
+	@GetMapping("/test")
+	public String test(){
+		return "/zul/test";
 	}
 }

f841561791443d33acd43a1a64dfc52555473ad0
hawkchen <hawkchen@potix.com>
2024-03-27 16:40:00 +0800

    add StatelessAutoConfigTest

---
+++ zkspringboot-autoconfig/src/test/java/org/zkoss/zkspringboot/test/StatelessAutoConfigTest.java
@@ -0,0 +1,41 @@
+package org.zkoss.zkspringboot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
+import org.zkoss.stateless.ui.http.DispatcherRichletFilter;
+import org.zkoss.zk.au.http.DHtmlUpdateServlet;
+import org.zkoss.zkspringboot.ZkProperties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:stateless-application.properties")
+public class StatelessAutoConfigTest {
+
+    @Autowired
+    ServletRegistrationBean<DHtmlUpdateServlet> dHtmlUpdateServlet;
+    @Autowired
+    ZkProperties zkProperties;
+    @Autowired
+    FilterRegistrationBean<DispatcherRichletFilter> dispatcherRichletFilterFilterRegistrationBean;
+
+    @Test
+    public void testDHtmlUpdateServlet() {
+        assertNotNull(dHtmlUpdateServlet);
+    }
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    public void testDispatcherRichletFilterBeanExists() {
+        assertNotNull(dispatcherRichletFilterFilterRegistrationBean, "FilterRegistrationBean<DispatcherRichletFilter> should not be null");
+        assertEquals("/stateless/*", dispatcherRichletFilterFilterRegistrationBean.getUrlPatterns().iterator().next());
+        assertEquals("org.zkoss.zkspringboot.demo.stateless", dispatcherRichletFilterFilterRegistrationBean.getInitParameters().get("basePackages"));
+    }
+}
+

def92f422a7f5ad8deca8ac3f79d6d37ac62a11f
hawkchen <hawkchen@potix.com>
2024-10-24 16:11:09 +0800

    add more examples for WAR packaging

--- zkspringboot-demos/zkspringboot-minimal-war/src/main/java/org/zkoss/zkspringboot/demo/MinimalWarApplication.java
+++ zkspringboot-demos/zkspringboot-minimal-war/src/main/java/org/zkoss/zkspringboot/demo/MinimalWarApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * If you start the application with this class' main(), web context root is src/main/resources/META-INF/resources
+ * If you run with mvn spring-boot:run, web context root is src/main/webapp
+ */
 @SpringBootApplication
+@Controller
 public class MinimalWarApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -15,5 +22,15 @@ public class MinimalWarApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinimalWarApplication.class);
+	}
+
+	@GetMapping("/minimal")
+	public String minimal(){
+		return "/minimal";
+	}
+
+	@GetMapping("/index")
+	public String index(){
+		return "/index";
 	}
 }
